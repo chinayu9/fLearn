73dcd97e11ab555fd63a355801dae6f3c627809e
yu9iang <chinayu9iang@gmail.com>
2017-09-26 17:12:40 +0800

    koa-static koa-views study

--- fKoa/app.js
+++ fKoa/app.js
@@ -1,6 +1,18 @@
 const Koa = require('koa');
 const app = new Koa();
-const router = require('koa-router')();
+const bodyparser = require('koa-bodyparser');
+const views = require('koa-views');
+
+
+app.use(bodyparser());
+app.use(require('koa-static')(__dirname + '/public'));
+app.use(views(__dirname + '/views',{
+	extension:'pug'
+}));
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
 //logger
 app.use(async (ctx, next)=>{
 	const start = new Date();
@@ -8,13 +20,16 @@ app.use(async (ctx, next)=>{
 	const ms = new Date() - start;
 	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
-router.get('/hello/:name',async (ctx, next)=>{
-	let name = ctx.params.name;
-	ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-});
-router.get('/',async (ctx,next)=>{
-	ctx.response.body = `<h1>Index</h1>`;
-});
-app.use(router.routes());
+
+//routes
+app.use(index.routes());
+app.use(users.routes());
+
 app.listen(3000);
 console.log('app started at port 3000...');
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+
+
---
+++ fKoa/routes/index.js
@@ -0,0 +1,8 @@
+const router = require('koa-router')();
+
+router.get('/',async (ctx,next)=>{
+	await ctx.render('index',{title:'Welcome'});
+});
+
+
+module.exports = router;
