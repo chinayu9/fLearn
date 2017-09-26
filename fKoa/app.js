f1cc3101b09ef42f39e2d2443b2179a28216801a
yu9iang <chinayu9iang@gmail.com>
2017-09-26 11:45:20 +0800

    add logger output

--- fKoa/app.js
+++ fKoa/app.js
@@ -1,5 +1,12 @@
 const Koa = require('koa');
 const app = new Koa();
+//logger
+app.use(async (ctx, next)=>{
+	const start = new Date();
+	await next();
+	const ms = new Date() - start;
+	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
 app.use(async (ctx, next)=>{
 	await next();
 	ctx.response.type = 'text/html';
