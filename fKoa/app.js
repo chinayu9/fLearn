e09953a4ca9cd0a09bfee33df577cb3403d0050a
yu9iang <chinayu9iang@gmail.com>
2017-09-26 13:44:17 +0800

    koa-router use

--- fKoa/app.js
+++ fKoa/app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const app = new Koa();
+const router = require('koa-router')();
 //logger
 app.use(async (ctx, next)=>{
 	const start = new Date();
@@ -7,10 +8,13 @@ app.use(async (ctx, next)=>{
 	const ms = new Date() - start;
 	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
-app.use(async (ctx, next)=>{
-	await next();
-	ctx.response.type = 'text/html';
-	ctx.response.body = '<h1>Hello,Koa2!</h1>';
+router.get('/hello/:name',async (ctx, next)=>{
+	let name = ctx.params.name;
+	ctx.response.body = `<h1>Hello, ${name}!</h1>`;
 });
+router.get('/',async (ctx,next)=>{
+	ctx.response.body = `<h1>Index</h1>`;
+});
+app.use(router.routes());
 app.listen(3000);
 console.log('app started at port 3000...');
