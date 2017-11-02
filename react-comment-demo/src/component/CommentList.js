401d226b3964b0cf3867cad64aae356b3e40cd23
yu9iang <chinayu9iang@gmail.com>
2017-11-02 15:28:25 +0800

    评论功能基础功能实现

--- react-comment-demo/src/component/CommentList.js
+++ react-comment-demo/src/component/CommentList.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import Comment from './Comment';
 class CommentList extends React.Component{
+	static defaultProps = {
+		comments:[]
+	};
 	render(){
+		const comments = this.props.comments;
 		return (
-			<div>CommentList</div>
+			<div className="comment-list">
+				{
+					comments.map((comment,i)=><Comment comment={comment} key={i} />)
+				}
+			</div>
 		);
 	}
 }
